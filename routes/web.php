d586fd49ae58af77528e257b62721ae5f7f9678c
tepe77 <prasaswo@gmail.com>
2024-01-29 15:36:06 +0700

    Tambah Menu Navbar

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Route::get('/Chat', function() {
+//     return view('vendor.Chatify.pages.app');
+// })->middleware(['auth','verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
